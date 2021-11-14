634b79a34a8d369bed49204131a90a8233dffa33
Szymon Bieganski <S.Bieganski@chello.nl>
2021-11-14 12:26:35 +0100

    mount image with user/groupd ID

--- mount_drive.sh
+++ mount_drive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 part=0;
+access="rw"
 
 while [ $# -gt 0 ]
 do
@@ -9,8 +10,10 @@ do
 			echo "Usage:";
 			echo "$0 image.img"
 			echo " -p|--part part ID";
+			echo " -r|--read-only restrict write access";
 			echo " -f|--folder destination folder";;
 		-p|--part) shift; part=${1};;
+		-r|--read_only) access="ro";;
 		-f|--folder) shift; folder=${1};;
 		*) img=${1};;
 	esac
@@ -25,17 +28,28 @@ while [ $p -gt 0 ]; do
 	((p=p-1))
 done
 
+imgsize=$(stat -c%s "$img")
+echo "image size ${imgsize}"
+
+if [ $((offset+slimit)) -gt ${imgsize} ]; then
+	echo "end of part ${part} exceeds image boundary"
+	((slimit=imgsize-offset-1))
+fi
+
 PoFoDrives="DEFGHIJKLMNOPQRSTUVWXYZ"
 driveID=${PoFoDrives:${part}:1}
 echo "size limit: ${slimit}"
 echo "offset    : ${offset}"
 echo "partID    : ${part}"
 echo "driveID   : ${driveID}"
-
 folder=${3-${img}_disk${driveID}}
 if [ "zz" == "z${3}z" ]; then
 	echo "creating folder: ${folder}"
-	mkdir ${folder}
+	mkdir -p ${folder}
 fi
+uid=`id -u`
+echo "user id   : ${uid}"
+gid=`id -g`
+echo "user gid  : ${gid}"
 
-sudo mount -o loop,offset=${offset},sizelimit=${slimit} ${img} ${folder} || rm -rf ${folder}
+sudo mount -o loop,${access},user,uid=${user_id},gid=${gid},offset=${offset},sizelimit=${slimit} ${img} ${folder} || rm -rf ${folder}
