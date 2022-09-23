006dd31312543605f7eb4a84031c9a7fd3900b2a
Szymon Bieganski <S.Bieganski@chello.nl>
2022-09-23 20:28:08 +0200

    specify foldername help printout

--- mount_drive.sh
+++ mount_drive.sh
@@ -2,16 +2,20 @@
 
 part=0;
 access="rw"
+folder=""
 
+function help {
+	echo "Usage:";
+	echo "$0 image.img"
+	echo " -p|--part part ID";
+	echo " -r|--read-only restrict write access";
+	echo " -f|--folder destination folder";
+	exit;
+}
 while [ $# -gt 0 ]
 do
 	case ${1} in
-		-h|--help)
-			echo "Usage:";
-			echo "$0 image.img"
-			echo " -p|--part part ID";
-			echo " -r|--read-only restrict write access";
-			echo " -f|--folder destination folder";;
+		-h|--help) help;;
 		-p|--part) shift; part=${1};;
 		-r|--read_only) access="ro";;
 		-f|--folder) shift; folder=${1};;
@@ -31,11 +35,13 @@ while [ $p -gt 0 ]; do
 done
 
 imgsize=$(stat -c%s "$img")
-echo "image size ${imgsize}"
 
-if [ $((offset+slimit)) -gt ${imgsize} ]; then
-	echo "end of part ${part} exceeds image boundary"
-	((slimit=imgsize-offset-1))
+if [ $imgsize -gt 0 ]; then
+	echo "image size ${imgsize}"
+	if [ $((offset+slimit)) -gt ${imgsize} ]; then
+		echo "end of part ${part} exceeds image boundary"
+		((slimit=imgsize-offset-1))
+	fi
 fi
 
 PoFoDrives="DEFGHIJKLMNOPQRSTUVWXYZ"
@@ -44,8 +50,10 @@ echo "size limit: ${slimit}"
 echo "offset    : ${offset}"
 echo "partID    : ${part}"
 echo "driveID   : ${driveID}"
-folder=${3-${img}_disk${driveID}}
-if [ "zz" == "z${3}z" ]; then
+if [ "xx" == "x${folder}x" ]; then
+	folder=${3-${img}_disk${driveID}}
+fi
+if [ "zz" == "z${folder}z" ]; then
 	echo "creating folder: ${folder}"
 	mkdir -p ${folder}
 fi
